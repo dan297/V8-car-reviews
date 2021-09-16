07349c37182e8b56d31a536a15cf4fada9e27788
jjcourtney <jcourtneywork@gmail.com>
2021-09-16 20:51:08 +0100

    added api routes

---
+++ controllers/api/index.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+const carRoutes = require('./carRoutes');
+const reviewRoutes = require('./reviewRoutes');
+const userRoutes = require('./userRoutes');
+
+
+router.use('/cars', carRoutes);
+
+router.use('/review', reviewRoutes);
+
+router.use('/users', userRoutes);
+
+
+
+module.exports = router;
