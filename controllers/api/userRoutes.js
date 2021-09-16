f4a398fe8b0f5836a6500b0461709ac734d4891f
Dan <Danielwhite98@icloud.com>
2021-09-16 21:44:01 +0100

    signup

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,17 +1,55 @@
-const { where } = require("sequelize/types");
+const router = require('express').Router();
+
+const { User } = require('../../models');
+
+
+// route for login to be added (get)
+// expect email and password to be on red.body
+router.post("/login", async (req, res) => {
+
+    const { email, password } = req.body;
+
+    const userData = await User.findOne({
+        where: {
+            email
+        }
+    })
+
+    if (!userData) {
+        res.status(400).json("Could not find username / password combination")
+    } else if (userData.password === password) {
+        console.log("Logged in")
+        res.status(200).json("logged in");
+        // this is where we write the code once logged in
+
+    } else {
+        res.status(400).json("Could not find username / password combination");
+    }
+});
+
+
+module.exports = router;
+
+
+// route for sign up (post)
+
 
 router.post("/", async (req, res) => {
     try {
-      const User = await User.create({ ...req.body});
+      const newUser = await User.create({ ...req.body, isInstructor: false });
   
       req.session.save(() => {
         req.session.logged_in = true;
-        req.session.user_id = User.id;
+        req.session.user_id = newUser.id;
   
+        const { password, ...safeProps } = newUser.get({ plain: true });
   
-        res.json({ user: User.id });
+        res.json({ user: safeProps });
       });
     } catch (error) {
       res.status(400).json(error);
     }
   });
+
+// get username from email (get)
+
