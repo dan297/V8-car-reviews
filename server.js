f2466522851df591607028f7042eb704b195a1a7
Dan <Danielwhite98@icloud.com>
2021-09-16 21:55:24 +0100

    serverjs changes

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express")
 const path = require("path")
 const dbConnection = require ('./config/connection')
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+  
+  app.use(session(sess));
 const app = express()
 
 const PORT = process.env.PORT || 3000
