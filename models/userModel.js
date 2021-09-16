aacac0ea250904689b3f7ebf05aefb168877f096
Dan <Danielwhite98@icloud.com>
2021-09-16 19:10:35 +0100

    user model

---
+++ models/userModel.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class User extends Model {}
+
+User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      first_name: {
+        type: DataTypes.STRING,
+      },
+      last_name: {
+        type: DataTypes.STRING,
+      },
+      accuracy_rating: {
+        type: DataTypes.INTEGER,
+      },
+      number_of_reviews: {
+        type: DataTypes.INTEGER,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+    },
+    
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          min: 8,
+        }},
+      username: {
+        type: DataTypes.STRING,
+      },
+      },
+    {
+      sequelize,
+      tableName: "user",
+    }
+
+  );
+  
+  module.exports = User;
