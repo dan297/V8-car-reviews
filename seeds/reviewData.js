5e0babb16294065722fbead5ff27f140beeeb31e
jjcourtney <jcourtneywork@gmail.com>
2021-09-16 19:39:11 +0100

    added seed data

--- seeds/reviewData.js
+++ seeds/reviewData.js
@@ -2,6 +2,22 @@ const { Review } = require('../models');
 
 const reviewData = [
     {
+        rating: 5,
+        description: "Great car",
+        car_id: 1,
+        user_id: 1
+    },
+    {
+        rating: 3,
+        description: "OK car",
+        car_id: 1,
+        user_id: 1
+    },
+    {
+        rating: 1,
+        description: "Rubbish car",
+        car_id: 1,
+        user_id: 1
     },
 ];
 
